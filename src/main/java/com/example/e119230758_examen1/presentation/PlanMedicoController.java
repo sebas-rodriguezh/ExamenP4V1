4e7f3b820b3078c3d870b4b08f05b9538be445a9
sebas-rodriguezh <sebastian.rodriguez.hernandez@est.una.ac.cr>
2026-03-28 10:39:29 -0600

    idea1

--- src/main/java/com/example/e119230758_examen1/presentation/PlanMedicoController.java
+++ src/main/java/com/example/e119230758_examen1/presentation/PlanMedicoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/presentation/planMedico")
+@RequestMapping("/presentation/planMedico") //Profe, esto lo usé para poder hacer los redirect de manera correcta.
 public class PlanMedicoController
 {
     @Autowired Service service;
